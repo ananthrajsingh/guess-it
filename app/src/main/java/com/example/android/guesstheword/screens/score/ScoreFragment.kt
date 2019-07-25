8da04bd207068dd82d05a17cf00ab0d6f9633bd9
ananthrajsingh <ananthraj.singh@gmail.com>
2019-07-25 18:04:44 +0530

    Complete Exercise

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -27,6 +27,7 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.GameFragmentBinding
 import com.example.android.guesstheword.databinding.ScoreFragmentBinding
 
 /**
@@ -36,6 +37,7 @@ class ScoreFragment : Fragment() {
 
     private lateinit var viewModel: ScoreViewModel
     private lateinit var viewModelFactory: ScoreViewModelFactory
+    private lateinit var binding: ScoreFragmentBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -44,7 +46,7 @@ class ScoreFragment : Fragment() {
     ): View? {
 
         // Inflate view and obtain an instance of the binding class.
-        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
                 inflater,
                 R.layout.score_fragment,
                 container,
@@ -57,15 +59,15 @@ class ScoreFragment : Fragment() {
         viewModel = ViewModelProviders.of(this, viewModelFactory)
                 .get(ScoreViewModel::class.java)
 
-        // TODO (06) Pass the ScoreViewModel into the data binding - then you can remove the
+        // COMPLETED (06) Pass the ScoreViewModel into the data binding - then you can remove the
         // OnClickListener setup for playAgainButton from here
 
+        binding.scoreViewModel = viewModel
         // Add observer for score
         viewModel.score.observe(this, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
         })
 
-        binding.playAgainButton.setOnClickListener { viewModel.onPlayAgain() }
 
         // Navigates back to title when button is pressed
         viewModel.eventPlayAgain.observe(this, Observer { playAgain ->
